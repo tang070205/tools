1e9621798e776b6b6422c2b2c36a1fc249e50e9c
tang070205 <tang070205@proton.me>
2024-08-08 22:54:25 +0800

    multiple kappa drawings

--- plot_kappa_multiple.py
+++ plot_kappa_multiple.py
@@ -33,7 +33,7 @@ def running_ave(y: np.ndarray, x: np.ndarray) -> np.ndarray:
 
 if sys.argv[2] == 'x' or sys.argv[2] == 'y':
     plt.figure(figsize=(17, 5))
-    def plot_running_avg(data, subplot_index, color, ylabel, ylimit, yticks, title_tag):
+    def plot_running_avg(data, subplot_index, color, ylabel, y_start, y_end, yticks, title_tag):
         ax = plt.subplot(1, 3, subplot_index)
         avg_data = np.zeros_like(data[0])
         for dataset in data:
@@ -44,9 +44,9 @@ if sys.argv[2] == 'x' or sys.argv[2] == 'y':
         plt.plot(t, avg_data, color=color, linewidth=2)
         plt.annotate(f'{avg_data[-1]:.2f}', xy=(t[-1], avg_data[-1]), xytext=(-20, 5), textcoords='offset points', ha='center', va='bottom')
         plt.xlim([0, xlimit])
-        plt.ylim([-50, ylimit])
+        plt.ylim([y_start, y_end])
         plt.gca().set_xticks(np.arange(0, xlimit+1, 1))
-        plt.gca().set_yticks(np.arange(-50, ylimit+1, yticks))
+        plt.gca().set_yticks(np.arange(y_start, y_end+1, yticks))
         plt.xlabel('time (ns)')
         plt.ylabel(ylabel)
         plt.title(f'({title_tag})')
@@ -57,8 +57,8 @@ if sys.argv[2] == 'x' or sys.argv[2] == 'y':
     elif sys.argv[2] == 'y':
         ki_data = [file_datas[i][:, 2] for i in range(int(sys.argv[1]))]
         ko_data = [file_datas[i][:, 3] for i in range(int(sys.argv[1]))]
-    plot_running_avg(ki_data, 1, 'red', r'$\kappa_{in}$ W/m/K', 100, 30, 'a')
-    plot_running_avg(ko_data, 2, 'blue', r'$\kappa_{out}$ W/m/K', 100, 30, 'b')
+    plot_running_avg(ki_data, 1, 'red', r'$\kappa_{in}$ W/m/K', -2000, 4000, 1000, 'a')
+    plot_running_avg(ko_data, 2, 'blue', r'$\kappa_{out}$ W/m/K', 0, 4000, 1000, 'b')
 
     plt.subplot(1, 3, 3)
     plt.plot(t, running_ave(np.mean(np.array(ki_data), axis=0),t), 'red', label='in', linewidth=2)
@@ -74,7 +74,6 @@ if sys.argv[2] == 'x' or sys.argv[2] == 'y':
     plt.ylabel(r'$\kappa_{total}$ W/m/K')
     plt.title('(c)')
     plt.legend(['in', 'out', 'total'])
-
     plt.savefig(f'hnemd-{sys.argv[2]}.png', dpi=150, bbox_inches='tight')
 
 elif sys.argv[2] == 'z':
@@ -85,9 +84,9 @@ elif sys.argv[2] == 'z':
         plt.plot(t, plot_data, color='C7', alpha=0.5)
     plt.plot(t, running_ave(np.mean(np.array(kz_data), axis=0),t), 'black', linewidth=2)
     plt.xlim([0, xlimit])
-    plt.ylim([0, 100])
+    plt.ylim([0, 4000])
     plt.gca().set_xticks(np.arange(0, xlimit+1, 1))
-    plt.gca().set_yticks(np.arange(0, 101, 20))
+    plt.gca().set_yticks(np.arange(0, 4001, 1000))
     plt.xlabel('time (ns)')
     plt.ylabel(r'$\kappa_{z}$ (W/m/K)')
 
