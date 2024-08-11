a64be549bab764f8f6383b56597eeba56dd901c7
tang070205 <tang070205@proton.me>
2024-08-11 23:54:41 +0800

    Update plot_kappa_multiple.py

--- plot_kappa_multiple.py
+++ plot_kappa_multiple.py
@@ -34,7 +34,7 @@ def running_ave(y: np.ndarray, x: np.ndarray) -> np.ndarray:
 
 if dic == 'x' or dic == 'y':
     plt.figure(figsize=(17, 5))
-    def plot_running_avg(data, subplot_index, color, ylabel, y_start, y_end, yticks, title_tag):
+    def plot_running_avg(data, subplot_index, color, ylabel, y_start, y_end, title_tag):
         ax = plt.subplot(1, 3, subplot_index)
         avg_data = np.zeros_like(data[0])
         for dataset in data:
@@ -46,8 +46,8 @@ if dic == 'x' or dic == 'y':
         plt.annotate(f'{avg_data[-1]:.2f}', xy=(t[-1], avg_data[-1]), xytext=(-20, 5), textcoords='offset points', ha='center', va='bottom')
         plt.xlim([0, xlimit])
         plt.ylim([y_start, y_end])
-        plt.gca().set_xticks(np.arange(0, xlimit+1, 1))
-        plt.gca().set_yticks(np.arange(y_start, y_end+1, yticks))
+        plt.gca().set_xticks(linspace(0, xlimit, 6))
+        plt.gca().set_yticks(linspace(y_start, y_end, 5))
         plt.xlabel('time (ns)')
         plt.ylabel(ylabel)
         plt.title(f'({title_tag})')
@@ -58,8 +58,8 @@ if dic == 'x' or dic == 'y':
     elif dic == 'y':
         ki_data = [file_datas[i][:, 2] for i in range(int(sys.argv[1]))]
         ko_data = [file_datas[i][:, 3] for i in range(int(sys.argv[1]))]
-    plot_running_avg(ki_data, 1, 'red', r'$\kappa_{in}$ W/m/K', -2000, 4000, 1000, 'a')
-    plot_running_avg(ko_data, 2, 'blue', r'$\kappa_{out}$ W/m/K', 0, 4000, 1000, 'b')
+    plot_running_avg(ki_data, 1, 'red', r'$\kappa_{in}$ W/m/K', -2000, 4000, 'a')
+    plot_running_avg(ko_data, 2, 'blue', r'$\kappa_{out}$ W/m/K', 0, 4000, 'b')
 
     plt.subplot(1, 3, 3)
     plt.plot(t, running_ave(np.mean(np.array(ki_data), axis=0),t), 'red', label='in', linewidth=2)
@@ -69,8 +69,8 @@ if dic == 'x' or dic == 'y':
     plt.annotate(f'{running_avg_k[-1]:.2f}', xy=(t[-1], running_avg_k[-1]), xytext=(-20, -10), textcoords='offset points', ha='center', va='bottom')
     plt.xlim([0,xlimit])
     plt.ylim([-50, 100])
-    plt.gca().set_xticks(np.arange(0, xlimit+1, 1))
-    plt.gca().set_yticks(np.arange(-50, 201, 50))
+    plt.gca().set_xticks(linspace(0, xlimit, 6))
+    plt.gca().set_yticks(linspace(-50, 200, 7))
     plt.xlabel('time (ns)')
     plt.ylabel(r'$\kappa_{total}$ W/m/K')
     plt.title('(c)')
@@ -86,8 +86,8 @@ elif dic == 'z':
     plt.plot(t, running_ave(np.mean(np.array(kz_data), axis=0),t), 'black', linewidth=2)
     plt.xlim([0, xlimit])
     plt.ylim([0, 4000])
-    plt.gca().set_xticks(np.arange(0, xlimit+1, 1))
-    plt.gca().set_yticks(np.arange(0, 4001, 1000))
+    plt.gca().set_xticks(linspace(0, xlimit, 6))
+    plt.gca().set_yticks(linspace(0, 4000, 5))
     plt.xlabel('time (ns)')
     plt.ylabel(r'$\kappa_{z}$ (W/m/K)')
 
