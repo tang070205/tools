529cfda2926a1f7f8bec472fe50300e50dbbbef1
tang070205 <tang070205@proton.me>
2024-08-12 10:35:35 +0800

    Update plot_compute_shc.py

--- plot_compute_shc.py
+++ plot_compute_shc.py
@@ -2,21 +2,37 @@ import sys
 import numpy as np
 from ase.io import read
 from pylab import *
+from ase.io import read,write
+from ase.build import graphene_nanoribbon
 
-with open('run.in', 'r') as file:
-    for line in file:
-        line = line.strip()
-        if 'heat_lan' in line:
-            t_start, t_end = int(line.split()[1]-line.split()[3]), int(line.split()[1]+line.split()[3])
-            group_first, group_last = int(line.split()[5]), int(line.split()[6])
-        elif 'compute_shc' in line:
-            num_corr_points = int(line.split()[2])
-            max_corr_t = int(line.split()[1]*line.split()[2]/1000)
-            dic = 'x' if  int(line.split()[2]) == 0 else 'y' if  int(line.split()[2]) == 1 else 'z'
-            num_omega = int(line.split()[4])
+gnr = graphene_nanoribbon(100, 101, type='armchair', sheet=True, vacuum=3.35/2)
+gnr.euler_rotate(theta=90)
+lx, lz, ly = gnr.cell.lengths()
+gnr.cell = gnr.cell.new((lx, ly, lz))
+gnr.center()
+gnr.pbc = [True, True, False]
+gnr
+
+group_cyclical = [1,20,10,10,10,10,10,10,20]
+def split_group(input_list):
+    return [n * 1.42 for n in input_list]
+ncounts = [400 * count for count in group_cyclical]
+split = split_group(group_cyclical)
+split = [0] + list(np.cumsum(split)) + [ly]
+print("y-direction boundaries:", [round(l,2) for l in split])
+print("atoms per group:", ncounts)
+group_id = []
+for atom in gnr:
+    n = atom.position[-2]
+    for i in range(len(split)):
+        if n > split[i] and n < split[i + 1]:
+            group_index = i
+    group_id.append(group_index)
+gnr.arrays["group"] = np.array(group_id)
+write("model.xyz", gnr)
 
 compute = np.loadtxt('compute.out')
-T = compute[:, group_first:group_last+1]
+T = compute[:, 1, 9]
 Ein = compute[:, -2]
 Eout = compute[:, -1]
 temp_ave = np.mean(T[1+int(len(compute)/2):,:], axis=0)
@@ -25,12 +41,12 @@ t = np.arange(1,len(compute)+1) * Ns/1000000  # ns
 
 figure(figsize=(10,5))
 subplot(1,2,1)
-group_idx = range(group_first, group_last+1)
+group_idx = range(1, 9)
 plot(group_idx, temp_ave,linewidth=3,marker='o',markersize=10)
-xlim([group_first, group_last])
+xlim([1,8])
 gca().set_xticks(group_idx)
-ylim([t_start, t_end])
-gca().set_yticks(linspace(t_start, t_end+1,6))
+ylim([290, 310])
+gca().set_yticks(linspace(290,310,6))
 xlabel('group index')
 ylabel('T (K)')
 title('(a)')
@@ -61,19 +77,18 @@ G = 160*Q/deltaT/A  # [GW/m2/K]
 shc = np.loadtxt('shc.out')
 Lx, Ly, Lz = l[0], 3*1.42*10, l[2]
 V = Lx * Ly * Lz
-Vvcf = shc[:2 * num_corr_points - 1, :]
+Vvcf = shc[:499, :]
 shc_t, shc_Ki, shc_Ko = Vvcf[:, 0], Vvcf[:, 1], Vvcf[:, 2]
-shc_nu = shc[-num_omega:, 0]/(2*pi)
-shc_jwi = shc[-num_omega:, 1]
-shc_jwo = shc[-num_omega:, 2]
+shc_nu = shc[-1000:, 0]/(2*pi)
+shc_jwi, shc_jwo = shc[-1000:, 1], shc[-1000:, 2]
 Gc = 1.6e4*(shc_jwi+shc_jwo)/V/deltaT
 
 figure(figsize=(10,5))
 subplot(1,2,1)
 L = Lx if sys.argv[2] == 'x' else Ly if sys.argv[2] == 'y' else Lz
 plot(shc_t, (shc_Ki+shc_Ko)/L, linewidth=2)
-xlim([-max_corr_t, max_corr_t])
-gca().set_xticks(linspace(-max_corr_t, max_corr_t, 5))
+xlim([-0.5, 0.5])
+gca().set_xticks(linspace(-0.5, 0.5, 5))
 ylim([-4, 10])
 gca().set_yticks(linspace(-4,10,9))
 ylabel('K (eV/ps)')
