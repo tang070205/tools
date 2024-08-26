fbb85d29b859a146a863f75bf368a3d237e1bc99
tang070205 <tang070205@proton.me>
2024-08-26 17:48:33 +0800

    Update create_perturb_vasp.py

--- create_perturb_vasp.py
+++ create_perturb_vasp.py
@@ -26,11 +26,11 @@ perturb_dic = f'perturb'
 os.makedirs(perturb_dic, exist_ok=True)
 shutil.copyfile(poscar_file, os.path.join(perturb_dic, 'CONTCAR'))
 os.chdir(perturb_dic)
-perturbed_system = dpdata.System('CONTCAR').perturb(pert_num=int(sys.argv[1]),
+perturbed_system = dpdata.System('CONTCAR').perturb(pert_num=perturbations,
                                                     cell_pert_fraction=0.03,
                                                     atom_pert_distance=0.15,
                                                     atom_pert_style='uniform')
-for j in range(int(sys.argv[1])):  
+for j in range(perturbations):  
     train_directory = f'train-{j+1}'
     os.makedirs(train_directory, exist_ok=True)
     poscar_filename = f'POSCAR{j+1}'
