77b50327beaf093d4e1bc47783584e307219ca62
tang070205 <tang070205@proton.me>
2024-08-26 16:39:28 +0800

    Update create_perturb_vasp.py

--- create_perturb_vasp.py
+++ create_perturb_vasp.py
@@ -22,37 +22,31 @@ if __name__ == "__main__":
 
 perturbations = int(sys.argv[1])
 original_cwd = os.getcwd()
-poscar_files = glob.glob('POSCAR-*')
+perturb_dic = f'perturb'
+os.makedirs(perturb_dic, exist_ok=True)
+shutil.copyfile(poscar_file, os.path.join(perturb_dic, 'CONTCAR'))
+os.chdir(perturb_dic)
+perturbed_system = dpdata.System('CONTCAR').perturb(pert_num=int(sys.argv[1]),
+                                                    cell_pert_fraction=0.03,
+                                                    atom_pert_distance=0.15,
+                                                    atom_pert_style='uniform')
+for j in range(perturbations):  
+    train_directory = f'train-{j+1}'
+    os.makedirs(train_directory, exist_ok=True)
+    poscar_filename = f'POSCAR{j+1}'
+    perturbed_system.to_vasp_poscar(poscar_filename, frame_idx=j)
+    shutil.move(poscar_filename, os.path.join(train_directory, 'POSCAR'))
+os.chdir('..')
 
-for poscar_file in poscar_files:
-    base_name = os.path.splitext(os.path.basename(poscar_file))[0]
-    perturb_directory = f'perturb-{base_name}'
-    os.makedirs(perturb_directory, exist_ok=True)
-    shutil.copyfile(poscar_file, os.path.join(perturb_directory, 'CONTCAR'))
-    os.chdir(perturb_directory)
-    perturbed_system = dpdata.System('CONTCAR').perturb(pert_num=int(sys.argv[1]),
-                                                        cell_pert_fraction=0.03,
-                                                        atom_pert_distance=0.15,
-                                                        atom_pert_style='uniform')
-    for j in range(perturbations):  
-        train_directory = f'train-{j+1}'
-        os.makedirs(train_directory, exist_ok=True)
-        poscar_filename = f'POSCAR{j+1}'
-        perturbed_system.to_vasp_poscar(poscar_filename, frame_idx=j)
-        shutil.move(poscar_filename, os.path.join(train_directory, 'POSCAR'))
-    os.chdir('..')
-os.chdir(original_cwd)
-
-for folder_name in os.listdir(original_cwd):
-    folder_path = os.path.join(original_cwd, folder_name)
-    if os.path.isdir(folder_path):
-        for subfolder_name in os.listdir(folder_path):
-            subfolder_path = os.path.join(folder_path, subfolder_name)
-            if os.path.isdir(subfolder_path):
-                shutil.copy("INCAR-single", os.path.join(subfolder_path, 'INCAR'))
-                os.chdir(subfolder_path)
-                vaspkit_command = "vaspkit -task 102 -kpr 0.03" 
-                subprocess.run(vaspkit_command, shell=True)
-                os.chdir(original_cwd)
-
+perturb_dic = f'perturb'
+folder_path = os.path.join(original_cwd, perturb_dic)
+if os.path.isdir(folder_path):
+    for subfolder_name in os.listdir(folder_path):
+        subfolder_path = os.path.join(folder_path, subfolder_name)
+        if os.path.isdir(subfolder_path):
+            shutil.copy("INCAR-single", os.path.join(subfolder_path, 'INCAR'))
+            os.chdir(subfolder_path)
+            vaspkit_command = "vaspkit -task 102 -kpr 0.03" 
+            subprocess.run(vaspkit_command, shell=True)
+            os.chdir(original_cwd)
 
