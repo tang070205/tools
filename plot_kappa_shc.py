10d29c8db6ec2e1c60c25cac08fa7030184e6ddf
tang070205 <tang070205@proton.me>
2024-09-10 21:42:03 +0800

    Update plot_kappa_shc.py

--- plot_kappa_shc.py
+++ plot_kappa_shc.py
@@ -115,7 +115,8 @@ lambda_i = (shc_kw/Gc)
 length = np.logspace(1,6,100)
 k_L = np.zeros_like(length)
 for i, el in enumerate(length):
-    k_L[i] = np.trapz(shc_kw/(1+lambda_i/el), shc_nu)
+    k_L[i] = np.trapz(shc_kw/(1+lambda_i/el), shc_nu)  #numpy<2
+    #k_L[i] = np.trapezoid(shc_kw/(1+lambda_i/el), shc_nu)  #numpy>=2
 
 figure(figsize=(12,10))
 subplot(2,2,1)
