08294d3365c298d336e87cc99aa8db4d6f0978dc
tang070205 <tang070205@proton.me>
2025-03-25 09:24:34 +0800

    Add allin function and specify the horizontal and vertical coordinates

--- get_nep_points2strucs.py
+++ get_nep_points2strucs.py
@@ -5,7 +5,8 @@ def main():
     if len(sys.argv) < 5:
         print("Usage: differ差值使用DFT-NEP")
         print("Usage: 目前有一个缺点,训练时如果不是fullbatch,需要将所有结构先预测一下,用那个out文件才能对的上xyz的结构顺序")
-        print("Usage: python get_energy_points.py direct <*.xyz> <*.out> x1_start x1_end x2_start x2_end...")
+        print("Usage: python get_energy_points.py direct <*.xyz> <*.out> nep/dft <x1/y1_start> <x1/y1_end> <x2/y2_start> <x2/y2_end>...")
+        print("Usage: python get_energy_points.py allin <*.xyz> <*.out> nep/dft <x/y_start> <x/y_end>")
         print("Usage: python get_energy_points.py differ <*.xyz> <*.out> <dif_value-> <dif_value+>")
         print("Usage: python get_energy_points.py rmse <*.xyz> <*.out> <max_rmse_strucs>")
         sys.exit(1)
@@ -24,26 +25,19 @@ for i in range(len(struc_atom)):
     struc_lines.append(struc_lines[-1] + struc_atom[i]+2)
 
 fout = np.loadtxt(sys.argv[3])
-if 'energy' in sys.argv[3]:
-    dir_value = fout[:,1]
-    dif_value = fout[:,1]-fout[:,0]
-    rmse = np.sqrt((fout[:,0]-fout[:,1])**2)
-elif 'force' in sys.argv[3]:
-    dir_value = fout[:,3:6]
-    dif_value = fout[:,3:6]-fout[:,0:3]
-    rmse = []
-    for i in range(len(atom_counts) - 1):
-        strucs_forces = fout[atom_counts[i]:atom_counts[i + 1], :]
-        strucs_rmse = np.sqrt(np.mean((strucs_forces[:,3:6]-strucs_forces[:,0:3])**2))
-        rmse.append(strucs_rmse)
-elif 'virial' in sys.argv[3]:
-    dir_value = fout[:,6:12]
-    dif_value = fout[:,6:12]-fout[:,0:6]
-    rmse = np.sqrt(np.sum((fout[:, 0:6] - fout[:, 6:12])**2, axis=1))
-elif 'stress' in sys.argv[3]:
-    dir_value = fout[:,6:12]
-    dif_value = fout[:,6:12]-fout[:,0:6]
-    rmse = np.sqrt(np.sum((fout[:, 0:6] - fout[:, 6:12])**2, axis=1))
+half_columns = int(fout.shape[1]//2)
+def calc_rmse(fout):
+    if 'energy' in sys.argv[3]:
+        return np.sqrt(np.mean((fout[:,:half_columns]-fout[:,half_columns:])**2))
+    elif 'force' in sys.argv[3]:
+        rmse = []
+        for i in range(len(atom_counts) - 1):
+            strucs_forces = fout[atom_counts[i]:atom_counts[i + 1], :]
+            strucs_rmse = np.sqrt(np.mean((strucs_forces[:,:half_columns]-strucs_forces[:,half_columns:])**2))
+            rmse.append(strucs_rmse)
+        return rmse
+    elif 'virial' or 'stress' in sys.argv[3]:
+        return np.sqrt(np.sum((fout[:,:half_columns]-fout[:,half_columns:])**2, axis=1))
 
 def force_struc_ids(di_ids, atom_counts):
     struc_ids = []
@@ -56,17 +50,34 @@ def force_struc_ids(di_ids, atom_counts):
     
 if sys.argv[1] == 'direct':
     direct_id = []
-    for i in range(4, len(sys.argv), 2):
+    dir_value = fout[:,:half_columns] if sys.argv[4] == 'nep' else fout[:,half_columns:]
+    for i in range(5, len(sys.argv), 2):
         small_value = float(sys.argv[i])
         large_value = float(sys.argv[i+1])
         dir_id = np.where((dir_value > small_value) & (dir_value < large_value))[0]
         direct_id.extend(dir_id)
-    if 'force' in sys.argv[3]:
+    if 'force' in sys.argv[4]:
         struc_id = sorted(set(force_struc_ids(direct_id, atom_counts)))
     else:
         struc_id = sorted(set(direct_id))
 
+elif sys.argv[1] == 'allin':
+    allin_id = []
+    if 'force' in sys.argv[3]:
+        for i in range(len(atom_counts) - 1):
+            strucs_forces = fout[atom_counts[i]:atom_counts[i + 1], :]
+            force_values = strucs_forces[:,:half_columns]if sys.argv[4] == 'nep' else strucs_forces[:,half_columns:]
+            if np.all(np.logical_and(force_values > float(sys.argv[4]), force_values < float(sys.argv[5]))):
+                allin_id.append(i)
+    else:
+        for idx, row in enumerate(fout):
+            allin_values = fout[:,:half_columns]if sys.argv[4] == 'nep' else fout[:,half_columns:]
+            if np.all(np.logical_and(allin_values > float(sys.argv[5]), allin_values < float(sys.argv[5]))):
+                allin_id.append(idx)
+    struc_id = sorted(set(allin_id))
+
 elif sys.argv[1] == 'differ':
+    dif_value = fout[:,:half_columns] - fout[:,half_columns:]
     small_value = float(sys.argv[4])
     large_value = float(sys.argv[5])
     differ_id = np.where((dif_value > large_value) | (dif_value < small_value))[0].tolist() 
@@ -76,6 +87,7 @@ elif sys.argv[1] == 'differ':
         struc_id = sorted(set(differ_id))
 
 elif sys.argv[1] == 'rmse':
+    rmse = calc_rmse(fout)
     max_rmse_strucs = int(sys.argv[4])
     rmse_id =np.argsort(np.array(rmse))
     struc_id = sorted(set(rmse_id[-max_rmse_strucs:]))
@@ -89,5 +101,4 @@ retain_id = [i for i in range(len(struc_atom)) if i not in struc_id]
 with open('reserve.xyz', 'w') as file2:
     for i, j in enumerate(retain_id):
         file2.writelines(lines[struc_lines[j]:struc_lines[j+1]])
-
 
