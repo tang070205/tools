80f3c1360f7a4b4001e7249c26c62b5e9b60670e
tang070205 <tang070205@proton.me>
2025-08-07 22:55:26 +0800

    Add selective drawing of atomic forces for individual element

--- plot_nep_results.py
+++ plot_nep_results.py
@@ -4,6 +4,7 @@ from pylab import *
 
 three_six_component = 0   # 0不画三六分量，1画三六分量
 use_range = 0   # 0使用默认读取文件最大值个最小值作范围，1使用对角线范围，2使用坐标轴范围
+element_force = 0   # 0不画元素力，1画元素力
 plot_range = {'energy': (-9, -8), 'force': (-20, 20), 'virial': (-10, 10), 
        'stress': (-10, 10), 'dipole': (-10, 10), 'polarizability': (-10, 10)}
 train_colors = ['red', 'green', 'blue', 'yellow', 'purple', 'cyan'] #力的话各取前三个
@@ -27,22 +28,6 @@ for file in files:
         vars()[file.split('.')[0]] = np.loadtxt(file)
 dipole_files, polar_files = glob.glob('dipole*'), glob.glob('polarizability*')
 model_type = 'dipole' if dipole_files else 'polarizability' if polar_files else None
-
-def set_tick_params():
-    tick_params(axis='x', which='both', direction='in', top=True, bottom=True)
-    tick_params(axis='y', which='both', direction='in', left=True, right=True)
-def get_counts2two(out_file):
-    file_nums = int(out_file.shape[1]//2)
-    new_nep, new_dft = out_file[:, :file_nums].flatten(), out_file[:, file_nums:].flatten()
-    return np.column_stack((new_nep, new_dft))
-def calc_r2_rmse(out_file):
-    file_columns = int(out_file.shape[1]//2)
-    numerator = np.sum((out_file[:, :file_columns] - out_file[:, file_columns:]) ** 2)
-    denominator = np.sum((out_file[:, :file_columns] - np.mean(out_file[:, :file_columns])) ** 2)
-    r2_data = 1.0 if denominator == 0 else 1 - (numerator / denominator)
-    rmse_origin = np.sqrt(np.mean((out_file[:, :file_columns]-out_file[:, file_columns:])**2))
-    rmse_data = rmse_origin * 1000 if rmse_origin < 1 else rmse_origin
-    return rmse_origin, rmse_data, r2_data
 
 in_file = 'gnep.in' if os.path.exists('gnep.in') else 'nep.in'
 lambda_v = 0.1
@@ -72,6 +57,76 @@ if lambda_v != '0':
         print("This index is only applicable to fullbatch training and prediction")
         virial_test, stress_test = virial_test[test_indices], stress_test[test_indices]
 
+def set_tick_params():
+    tick_params(axis='x', which='both', direction='in', top=True, bottom=True)
+    tick_params(axis='y', which='both', direction='in', left=True, right=True)
+
+def get_counts2two(out_file):
+    file_nums = int(out_file.shape[1]//2)
+    new_nep, new_dft = out_file[:, :file_nums].flatten(), out_file[:, file_nums:].flatten()
+    return np.column_stack((new_nep, new_dft))
+
+def calc_r2_rmse(out_file):
+    file_columns = int(out_file.shape[1]//2)
+    numerator = np.sum((out_file[:, :file_columns] - out_file[:, file_columns:]) ** 2)
+    denominator = np.sum((out_file[:, :file_columns] - np.mean(out_file[:, :file_columns])) ** 2)
+    r2_data = 1.0 if denominator == 0 else 1 - (numerator / denominator)
+    rmse_origin = np.sqrt(np.mean((out_file[:, :file_columns]-out_file[:, file_columns:])**2))
+    rmse_data = rmse_origin * 1000 if rmse_origin < 1 else rmse_origin
+    return rmse_origin, rmse_data, r2_data
+
+def plot_value(values, color):
+    columns = int(values.shape[1]//2)
+    if three_six_component == 0 or data == 'energy':
+        plot(values[:, 1], values[:, 0], '.', color=color)
+    else:
+        for i in range(columns):
+            plot(values[:, i+columns], values[:, i], '.', color=color[i % len(color)])
+    pass
+
+units = {'force': 'eV/Å', 'stress': 'GPa', 'energy': 'eV/atom','virial': 'eV/atom', 'dipole': 'a.u./atom', 'polarizability': 'a.u./atom'}
+munits = {'force': 'meV/Å', 'stress': 'MPa', 'energy': 'meV/atom','virial': 'meV/atom', 'dipole': 'ma.u./atom', 'polarizability': 'ma.u./atom'}
+def get_unit(data, rmse_origin):
+    return munits.get(data, 'unknown unit') if rmse_origin < 1 else units.get(data, 'unknown unit')
+
+def generate_dirs(data, types, prefixes):
+    comps = {3: ['x', 'y', 'z'], 6: ['xx', 'yy', 'zz', 'xy', 'yz', 'xz']}
+    if os.path.exists(f"{data}_test.out"):
+        return {typ: [f"{prefix}_{comp}" for comp in comps[3 if typ in ['force', 'dipole'] else 6]] for typ in types for prefix in prefixes}
+    else:
+        return {typ: [f"{comp}" for comp in comps[3 if typ in ['force', 'dipole'] else 6]] for typ in types}
+properties, prefixes = ['force', 'stress', 'virial', 'dipole', 'polarizability'], ['train', 'test']
+
+def get_range(data, data_file):
+    if data == 'energy':
+        return np.floor(data_file.min() * 10) / 10, np.ceil(data_file.max() * 10) / 10
+    else:
+        return np.floor(data_file.min()), np.ceil(data_file.max())
+        
+def process_data(data, data_type):
+    if three_six_component == 0:
+        return globals().get(f"{data}_{data_type}") if data == 'energy' else get_counts2two(globals().get(f"{data}_{data_type}"))
+    else:
+        return globals().get(f"{data}_{data_type}")
+
+def get_element_property(file, atoms_property):
+    potential_file = 'gnep.txt' if os.path.exists('gnep.txt') else 'nep.txt'
+    with open(potential_file, 'r') as txtfile:
+        first_line = txtfile.readline().strip()
+        elements = first_line.split()[2:]
+
+    from ase.io import read
+    atoms = read(f'{file}.xyz', index=':')
+    atom_symbols = []
+    for atom in atoms:
+        atom_symbol = atom.get_chemical_symbols()
+        atom_symbols.extend(atom_symbol)
+    element_lists = {element: [] for element in elements}
+    for symbol, atom_property in zip(atom_symbols, atoms_property):
+        element_lists[symbol].append(atom_property)
+    non_empty_elements_lists = {element: atom_property for element, atom_property in element_lists.items() if atom_property}
+    return non_empty_elements_lists
+
 def plot_loss():
     label_Lgnep, label_Lnep = [r'$L_{\text{total}}$'], [r'$L_1$', r'$L_2$']
     label_ef, label_ef_train, label_ef_test = ['Energy', 'Force'], ['E-train', 'F-train'], ['E-test', 'F-test']
@@ -92,7 +147,7 @@ def plot_loss():
         else:
             legend(label_l12 + [f'{model_type}'], ncol=3, frameon=False, fontsize=10, loc='upper right')
     else: 
-        if lambda_v == '0' or (train_novirial_indices is not None and len(train_novirial_indices) == len(virial_train)):
+        if lambda_v == '0' or (train_novirial_indices is not None and len(train_novirial_indices) == len(energy_train)):
             loglog(loss_L)
             loglog(loss_train)
             if os.path.exists('test.xyz'):
@@ -134,52 +189,21 @@ def plot_learning_rate():
 
 def plot_diagonal(data):
     color_train, color_test = generate_colors(data)
-    def plot_value(values, color):
-        columns = int(values.shape[1]//2)
-        if three_six_component == 0 or data == 'energy':
-            plot(values[:, 1], values[:, 0], '.', color=color)
-        else:
-            for i in range(columns):
-                plot(values[:, i+columns], values[:, i], '.', color=color[i % len(color)])
-    pass
-
-    units = {'force': 'eV/Å', 'stress': 'GPa', 'energy': 'eV/atom','virial': 'eV/atom', 'dipole': 'a.u./atom', 'polarizability': 'a.u./atom'}
-    munits = {'force': 'meV/Å', 'stress': 'MPa', 'energy': 'meV/atom','virial': 'meV/atom', 'dipole': 'ma.u./atom', 'polarizability': 'ma.u./atom'}
     label_unit = units.get(data, 'unknown unit')
-    def get_unit(rmse_origin):
-        return munits.get(data, 'unknown unit') if rmse_origin < 1 else units.get(data, 'unknown unit')
-    def generate_dirs(types, prefixes):
-        comps = {3: ['x', 'y', 'z'], 6: ['xx', 'yy', 'zz', 'xy', 'yz', 'xz']}
-        if os.path.exists(f"{data}_test.out"):
-            return {typ: [f"{prefix}_{comp}" for comp in comps[3 if typ in ['force', 'dipole'] else 6]] for typ in types for prefix in prefixes}
-        else:
-            return {typ: [f"{comp}" for comp in comps[3 if typ in ['force', 'dipole'] else 6]] for typ in types}
-    properties, prefixes = ['force', 'stress', 'virial', 'dipole', 'polarizability'], ['train', 'test']
-    train_dirs, test_dirs = generate_dirs(properties, prefixes[0::2]), generate_dirs(properties, prefixes[1::2])
+    train_dirs, test_dirs = generate_dirs(data, properties, prefixes[0::2]), generate_dirs(data, properties, prefixes[1::2])
     train_dir, test_dir = train_dirs.get(data, 'unknown train_dirs'), test_dirs.get(data, 'unknown test_dirs')
 
-    def get_range(data_file):
-        if data == 'energy':
-            return np.floor(data_file.min() * 10) / 10, np.ceil(data_file.max() * 10) / 10
-        else:
-            return np.floor(data_file.min()), np.ceil(data_file.max())
-    def process_data(data_type):
-        if three_six_component == 0:
-            return globals().get(f"{data}_{data_type}") if data == 'energy' else get_counts2two(globals().get(f"{data}_{data_type}"))
-        else:
-            return globals().get(f"{data}_{data_type}")
-
-    data_train = process_data('train')
-    train_min, train_max = get_range(data_train)
+    data_train = process_data(data, 'train')
+    train_min, train_max = get_range(data, data_train)
     plot_value(data_train, color_train)
     origin_rmse_train, rmse_data_train, r2_data_train = calc_r2_rmse(data_train)
 
     if os.path.exists(f"{data}_test.out"):
-        data_test = process_data('test')
-        test_min, test_max = get_range(data_test)
+        data_test = process_data(data, 'test')
+        test_min, test_max = get_range(data, data_test) 
         plot_value(data_test, color_test)
         origin_rmse_test, rmse_data_test, r2_data_test = calc_r2_rmse(data_test)
-        unitest = get_unit(origin_rmse_test)
+        unitest = get_unit(data, origin_rmse_test)
         if three_six_component == 0 or data == 'energy':
             legend([f'train RMSE= {rmse_data_train:.3f} {unitest} R²= {r2_data_train:.3f}', 
                    f'test RMSE= {rmse_data_test:.3f} {unitest} R²= {r2_data_test:.3f}'], frameon=False, fontsize=10)
@@ -189,13 +213,13 @@ def plot_diagonal(data):
             annotate(f'test RMSE= {rmse_data_test:.3f} {unitest} R²= {r2_data_test:.3f}', xy=(0.09, 0.92), fontsize=10, xycoords='axes fraction', ha='left', va='top')
     else:
         test_min, test_max = None, None
-        unitrain = get_unit(origin_rmse_train)
+        unitrain = get_unit(data, origin_rmse_train)
         if three_six_component == 0 or data == 'energy':
             legend([f'train RMSE= {rmse_data_train:.3f} {unitrain} R²= {r2_data_train:.3f}'], frameon=False, fontsize=10)
         else:
             legend(train_dir, frameon=False, fontsize=10, loc='upper left', bbox_to_anchor=(0, 0.95))
             annotate(f'train RMSE= {rmse_data_train:.3f} {unitrain} R²= {r2_data_train:.3f}', xy=(0.11, 0.97), fontsize=10, xycoords='axes fraction', ha='left', va='top')
-    
+
     if use_range == 0:
         range_min = train_min if test_min is None or train_min < test_min else test_min
         range_max = train_max if test_max is None or train_max > test_max else test_max
@@ -205,7 +229,7 @@ def plot_diagonal(data):
         range_min, range_max = plot_range.get(data, (None, None))
         xlim(range_min, range_max)
         ylim(range_min, range_max)
-    plot(linspace(range_min, range_max), linspace(range_min, range_max), '-')
+    plot(linspace(range_min, range_max), linspace(range_min, range_max), 'k--', zorder=0)
     set_tick_params()
     xlabel(f"DFT {data} ({label_unit})")
     ylabel(f"NEP {data} ({label_unit})")
@@ -219,38 +243,21 @@ def plot_charge():
         print('If it is not fullbatch, please use the predicted charge_ *. out file')
         return
     
-    from ase.io import read
     import seaborn as sns
-    txt_file = 'gnep.txt' if os.path.exists('gnep.txt') else 'nep.txt'
-    with open(txt_file, 'r') as file:
-        first_line = file.readline().strip()
-        elements = first_line.split()[2:]
     
     def sturges_bins(data):
         n = len(data)
         bins = int(math.log2(n) + 1)
         return max(bins, 1)
-    
-    def get_charge(file, charge_data):
-        charge_strucs = read(f'{file}.xyz', index=':')
-        charge_symbols = []
-        for charge_struc in charge_strucs:
-            charge_symbol = charge_struc.get_chemical_symbols()
-            charge_symbols.extend(charge_symbol)
-        element_charges = {element: [] for element in elements}
-        for symbol, charge in zip(charge_symbols, charge_data):
-            element_charges[symbol].append(-charge)
-        non_empty_elements_charges = {element: charges for element, charges in element_charges.items() if charges}
-        return non_empty_elements_charges
 
     figure(figsize=(5.5,5))
-    element_charges_train = get_charge('train', charge_train)
+    element_charges_train = get_element_property('train', -charge_train)
     if not os.path.exists("charge_test.out"):
         for element in element_charges_train.keys():
             sns.histplot(element_charges_train[element], bins=500, alpha=0.6, label=element, kde=True, line_kws={'lw': 1})
         legend(frameon=False, fontsize=10, loc='upper right')
     else:
-        element_charges_test = get_charge('test', charge_test)
+        element_charges_test = get_element_property('test', -charge_test)
         for element_train, element_test in zip(element_charges_train.keys(), element_charges_test.keys()):
             sns.histplot(element_charges_train[element_train], bins=500, alpha=0.6, label=f'{element_train}-train', kde=True, line_kws={'lw': 1})
             sns.histplot(element_charges_test[element_test], bins=500, alpha=0.6, label=f'{element_test}-test', kde=True, line_kws={'lw': 1})
@@ -261,7 +268,7 @@ def plot_charge():
     #ylim(0, 1000)
     set_tick_params()
     tight_layout()
-    savefig(f'nep-charge.png', dpi=200)
+    savefig(f'nep-charge.png', dpi=200, bbox_inches='tight')
     pass
 
 def plot_descriptor():
@@ -281,19 +288,7 @@ def plot_descriptor():
         cbar.set_label('E/atom (eV)')
         title('Descriptors for each structure')
     elif len(descriptor) == len(force_train):
-        def get_atom_des(file, descriptor):
-            des_strucs = read(f'{file}.xyz', index=':')
-            des_symbols = []
-            for des_struc in des_strucs:
-                des_symbol = des_struc.get_chemical_symbols()
-                des_symbols.extend(des_symbol)
-            element_descriptors = {element: [] for element in elements}
-            for symbol, des in zip(des_symbols, descriptor):
-                element_descriptors[symbol].append(des)
-            non_empty_elements_descriptors = {element: des for element, des in element_descriptors.items() if des}
-            return non_empty_elements_descriptors
-
-        element_des = get_atom_des('train', descriptor)
+        element_des = get_element_property('train', descriptor)
         for element in element_des.keys():
             scatter([i[0] for i in element_des[element]], [i[1] for i in element_des[element]], edgecolor='grey', alpha=0.8, label=element)
         legend(frameon=False, fontsize=10, loc='upper right')
@@ -304,10 +299,48 @@ def plot_descriptor():
     ylabel('PC2')
     set_tick_params()
     tight_layout()
-    savefig(f'nep-descriptor.png', dpi=200)
+    savefig(f'nep-descriptor.png', dpi=200, bbox_inches='tight')
     pass
 
-diag_types, type_vs = ['energy', 'force'], ['virial', 'stress']
+def plot_element_force():
+    force_elements_trian = get_element_property('train', force_train)
+    force_elements_test = get_element_property('test', force_test) if os.path.exists('force_test.out') else None
+
+    for element, force_element_train in force_elements_trian.items():
+        figure(figsize=(5.5, 5))
+        if os.path.exists('force_test.out'):
+            train_element_force = get_counts2two(np.array(force_element_train))
+            test_element_force = get_counts2two(np.array(force_elements_test[element]))
+            plot(train_element_force[:, 1], train_element_force[:, 0], '.', label=f'{element}-train', alpha=0.8, color='deepskyblue')
+            plot(test_element_force[:, 1], test_element_force[:, 0], '.', label=f'{element}-test', alpha=0.8, color='orange')
+        else:
+            train_element_force = np.array(force_element_train)
+            plot(train_element_force[:, 3], train_element_force[:, 0], '.', label=f'{element}-x', alpha=0.8, color='red')
+            plot(train_element_force[:, 4], train_element_force[:, 1], '.', label=f'{element}-y', alpha=0.8, color='green')
+            plot(train_element_force[:, 5], train_element_force[:, 2], '.', label=f'{element}-z', alpha=0.8, color='blue')
+        
+        train_min, train_max = get_range('force', train_element_force)
+        test_min, test_max = get_range('force', test_element_force) if os.path.exists('force_test.out') else (None, None)
+        if use_range == 0:
+            range_min = train_min if test_min is None or train_min < test_min else test_min
+            range_max = train_max if test_max is None or train_max > test_max else test_max
+        elif use_range == 1:
+            range_min, range_max = plot_range.get('force', (None, None))
+        elif use_range == 2:
+            range_min, range_max = plot_range.get('force', (None, None))
+            xlim(range_min, range_max)
+            ylim(range_min, range_max)
+        plot(linspace(range_min, range_max), linspace(range_min, range_max), 'k--', zorder=0)
+        set_tick_params()
+        xlabel('DFT force (eV/Å)')
+        ylabel('NEP force (eV/Å)')
+        legend(frameon=False, fontsize=10, loc='upper left')
+        tight_layout()
+        title(f'The force of element {element}')
+        savefig(f'{element}-force.png', dpi=300, bbox_inches='tight')
+    pass
+
+base_diag_types= ['energy', 'force', 'virial', 'stress']
 def plot_diagonals(diag_types, hang, lie, start):
     for i, diag_type in enumerate(diag_types):
         subplot(hang, lie, i+start)
@@ -317,21 +350,19 @@ def plot_base_picture():
     if model_type is not None:
         figure(figsize=(5.5,5))
         plot_diagonal(f'{model_type}')
-        savefig(f'nep-{model_type}.png', dpi=200)
+        savefig(f'nep-{model_type}-diagonal.png', dpi=200)
     else:
-        if lambda_v == '0' or (train_novirial_indices is not None and len(train_novirial_indices) == len(virial_train)):
+        if lambda_v == '0' or (train_novirial_indices is not None and len(train_novirial_indices) == len(energy_train)):
             figure(figsize=(11,5))
-            plot_diagonals(diag_types, 1, 2, 1)
+            plot_diagonals(base_diag_types[:2], 1, 2, 1)
             savefig('nep-ef-diagonals.png', dpi=200)
         elif not os.path.exists('stress_train.out'):
-            diag_types_v = diag_types + [type_vs[0]]
             figure(figsize=(16.5,5))
-            plot_diagonals(diag_types_v, 1, 3, 1)
+            plot_diagonals(base_diag_types[:3], 1, 3, 1)
             savefig('nep-efv-diagonals.png', dpi=200)
         else:
             figure(figsize=(11,10))
-            diag_types_vs = diag_types + type_vs
-            plot_diagonals(diag_types_vs, 2, 2, 1)
+            plot_diagonals(base_diag_types, 2, 2, 1)
             savefig('nep-efvs-diagonals.png', dpi=200)
 
 if os.path.exists('loss.out'):
@@ -349,9 +380,10 @@ if os.path.exists('loss.out'):
         savefig('nep-loss.png', dpi=200)
     plot_base_picture()
     plot_charge()
+    plot_element_force() if element_force == 1 else None
 else:
     print('NEP Prediction')
     plot_base_picture()
     plot_charge()
     plot_descriptor()
-
+    plot_element_force() if element_force == 1 else None
