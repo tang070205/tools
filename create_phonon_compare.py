8adba504bf9cde049e4eb17ec7bd25003a4a7751
tang070205 <tang070205@proton.me>
2026-03-14 15:37:14 +0800

    Update for GPUMD 5

--- create_phonon_compare.py
+++ create_phonon_compare.py
@@ -1,119 +1,60 @@
-import os, re
-import numpy as np
+import os
 from pylab import *
-from ase.io import read,write
+from ase.io import read
 
-cx,cy,cz = 20, 20, 1   # 超胞参数
-npoints = 300
-special_points = {'G': [0, 0, 0], 'K': [0.3333, 0.3333, 0], 'M': [0.5, 0, 0]}  # 高对称点坐标，同样vaspkit305提供的文件里有
-points_path = ['GMKG']     #高对称点路径，可以写断点比如['GM', 'KG']
-dft_file = 'phonon.out'  # DFT计算得到的声子频率文件，没有phonopy-bandplot --gnuplot > phonon.out这样生成phonon.out
-uc = read('POSCAR') #xyz、cif文件也可以
-struc = uc * (cx,cy,cz)
-write("model.xyz", struc)
+uc = read("model.xyz")   #必须是原胞
+kpoints = ['GMKG']     #高对称点路径，可从vaspkit305等获取，可以写断点比如['GM', 'KG']
+dft_file = 'phonon.out'
+y_max = 10
+# kpoints.in文件也可手写
+with open('kpoints.in', 'w') as f:
+    for kp in kpoints:
+        path = uc.cell.bandpath(path=kp, npoints=0)
+        for label in kp:
+            k = path.special_points[label]
+            f.write(f"{k[0]:.3f} {k[1]:.3f} {k[2]:.3f} {label}\n")
+        f.write("\n")
 
-with open('basis.in', 'w') as f:
-    f.write(f"{len(uc)}\n")
-    for i, mass in enumerate(uc.get_masses()):
-        f.write(f"{i} {mass}\n")
-    for _ in range(cx*cy*cz): 
-        for i in range(len(uc)):
-            f.write(f"{i}\n")
+with open("omega2.out", 'r') as f:
+    first_line = f.readline().strip().lstrip('#').split()
+    sym_points = first_line[:len(first_line)//2]
+    sym_labels = first_line[len(first_line)//2:]
+sym_points = [float(x) for x in sym_points]
+sym_labels = [r'$\Gamma$' if x == 'G' else x for x in sym_labels]
 
-npaths = len(points_path)
-lengths = [len(path) - 1 for path in points_path]
-path_ratio = [float(length / sum(lengths)) for length in lengths]
-if npoints % sum(lengths) != 0:
-    raise ValueError(f"npoints 建议是 {sum(lengths) * 100} 的倍数")
+omega2 = np.loadtxt("omega2.out")
+npath = omega2[:, 0]
+nu = omega2[:, 1:]
+for i in range(len(nu)):
+    for j in range(len(nu[0])):
+        nu[i, j] = np.sqrt(abs(nu[i, j])) / (2 * np.pi) * np.sign(nu[i, j])
 
-def generate_set_path(points_path):
-    segs = []
-    for seg in points_path:
-        tokens = re.findall(r'([A-Za-z])(\d*)', seg.upper())
-        segs.append([r'$\Gamma$' if L == 'G' else f'${L}_{{{N}}}$' if N else L for L, N in tokens])
+figure(figsize=(8,8))
 
-    labels = segs[0][:]
-    for i in range(1, len(segs)):
-        prev_tail = labels[-1]
-        curr_head = segs[i][0]
-        labels[-1] = f'{prev_tail}|{curr_head}'
-        labels.extend(segs[i][1:])
-
-    return labels
-
-gpumd_kpts, kpaths, sym_points_list, labels_list = [], [], [], []
-for i in range(npaths):
-    path = uc.cell.bandpath(path=points_path[i], npoints=path_ratio[i]*npoints, special_points=special_points)
-    kpath, sym_points, labels = path.get_linear_kpoint_axis()
-    kpaths.append(kpath)
-    sym_points_list.append(sym_points)
-    labels_list.append(labels)
-    kpts = np.matmul(path.kpts, uc.cell.reciprocal() * 2 * np.pi)
-    kpts[np.abs(kpts) < 1e-15] = 0.0
-    gpumd_kpts = kpts if i == 0 else np.vstack((gpumd_kpts, kpts))
-np.savetxt('kpoints.in', gpumd_kpts, header=str(npoints), comments='', fmt='%g')
-
-whole_kpaths, whole_sym_points = [], []
-origin_kpaths, origin_sym_points = 0, 0
-for i in range(len(kpaths)):
-    if i == 0:
-        whole_kpaths.extend(kpaths[i])
-        origin_kpaths = kpaths[i][-1]
-        whole_sym_points.extend(sym_points_list[i])
-        origin_sym_points = sym_points_list[i][-1]
-    else:
-        adjusted_kpaths = [x + origin_kpaths for x in kpaths[i]]
-        whole_kpaths.extend(adjusted_kpaths)
-        origin_kpaths += kpaths[i][-1]
-        adjusted_sym_points = [x + origin_sym_points for x in sym_points_list[i][1:]]
-        whole_sym_points.extend(adjusted_sym_points)
-        origin_sym_points += sym_points_list[i][-1]
-
-data = np.loadtxt("omega2.out")
-for i in range(len(data)):
-    for j in range(len(data[0])):
-        data[i, j] = np.sqrt(abs(data[i, j])) / (2 * np.pi) * np.sign(data[i, j])
-nep = data
-
-figure(figsize=(8, 7))
 if os.path.exists(dft_file):
     dft = np.loadtxt(dft_file)
     idx0 = np.where(dft[:, 0] == 0.000000)[0]
     idx0 = np.append(idx0, len(dft))
     blocks = [dft[:, 1][idx0[i]:idx0[i+1]] for i in range(len(idx0)-1)]
     DFT = np.column_stack(blocks)
-    dft_path = dft[idx0[0]:idx0[1],0] / dft[-1,0] * whole_kpaths[-1]
-    plot(dft_path, DFT, color='C1', lw=1)
+    dft_path = dft[idx0[0]:idx0[1],0] / dft[-1,0] * npath [-1]
+    plot(dft_path, DFT, color='C0', lw=2)
 
-plot(whole_kpaths, nep, color='C0', lw=1)
-xlim([0, whole_kpaths[-1]])
-for sym_point in whole_sym_points[1:-1]:
-    plt.axvline(sym_point, color='black', linestyle='--')
-gca().set_xticks(whole_sym_points)
-gca().set_xticklabels(generate_set_path(points_path))
-ylim([0, 10])
-gca().set_yticks(linspace(0,10,6))
-ylabel(r'$\nu$ (THz)',fontsize=15)
+plot(npath, nu, color='C1',lw=2)
+xlim([0, npath[-1]]);xticks(fontsize=18)
+vlines(sym_points[1:-1], ymin=0, ymax=y_max, color='black', linestyle='--')
+gca().set_xticks(sym_points)
+gca().set_xticklabels(sym_labels)
+ylim([0, y_max]);yticks(fontsize=18)
+ylabel(r'$\nu$ (THz)', fontsize=18)
 tick_params(axis='x', which='both', direction='in', top=True, bottom=True)
 tick_params(axis='y', which='both', direction='in', left=True, right=True)
 if os.path.exists(dft_file):
-    plot([], [], color='C1', lw=1, label='DFT')
-    plot([], [], color='C0', lw=1, label='NEP')
+    plot([], [], color='C0', lw=2, label='DFT')
+    plot([], [], color='C1', lw=2, label='NEP')
     legend()
+    savefig('phonon_NEPvsDFT.png', dpi=300, bbox_inches='tight')
 else:
-    plot([], [], color='C0', lw=1, label='NEP')
+    plot([], [], color='C1', lw=2, label='NEP')
     legend()
-savefig('phonon.png', dpi=300, bbox_inches='tight')
-
-group_velocity = np.gradient(nep, whole_kpaths, axis=0)
-figure(figsize=(9, 8))
-plot(nep.flatten(), np.abs(group_velocity.flatten()) / 5 * np.pi, '.')
-xlim([0, 10])
-gca().set_xticks(linspace(0, 10, 6))
-ylim([0, 10])
-gca().set_yticks(linspace(0, 10, 6))
-xlabel(r'$\nu$ (THz)', fontsize=15)
-ylabel(r'Group Velocity (km/s)', fontsize=15)
-tick_params(axis='x', which='both', direction='in', top=True, bottom=True)
-tick_params(axis='y', which='both', direction='in', left=True, right=True)
-savefig('phonon_with_group_velocity.png', dpi=300, bbox_inches='tight')
+    savefig('phonon_NEP.png', dpi=300, bbox_inches='tight')
